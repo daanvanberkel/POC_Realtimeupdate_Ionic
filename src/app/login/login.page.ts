e908140ea89bfdc092ad03fa9c05ec5863d307dd
Daan van Berkel <info@daanvanberkel.nl>
2020-02-08 15:08:30 +0100

    Avans SSO working on android

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {environment} from '../../environments/environment';
-import {Plugins, registerWebPlugin} from '@capacitor/core';
+import {AppLaunchUrl, AppUrlOpen, Plugins, registerWebPlugin} from '@capacitor/core';
 import {OAuth2Client} from '@daanvanberkel/capacitor-oauth2';
 import {LoginService} from '../services/login.service';
 import {Router} from '@angular/router';
+import {Platform} from '@ionic/angular';
+
+const { App } = Plugins;
 
 @Component({
   selector: 'app-login',
@@ -14,7 +17,8 @@ export class LoginPage implements OnInit {
 
   constructor(
       private loginService: LoginService,
-      private router: Router
+      private router: Router,
+      private platform: Platform,
   ) { }
 
   ngOnInit() {
@@ -23,29 +27,54 @@ export class LoginPage implements OnInit {
       return;
     }
 
+    if (this.isAndroid()) {
+      // Handle Deep Links on android
+      App.addListener('appUrlOpen', this.handleOpenUrl);
+      App.getLaunchUrl().then(this.handleOpenUrl);
+    }
+
+    // Register OAuth2client plugin with Capacitor
     registerWebPlugin(OAuth2Client);
 
     const url = new URL(window.location.href);
 
+    // Handle authentication
     Plugins.OAuth2Client.authenticate({
-      appId: 'speedmeet',
       authorizationBaseUrl: `${environment.api_base}/auth/avans`,
       responseType: 'token',
       web: {
+        appId: this.isAndroid() ? 'speedmeetand' : 'speedmeetweb',
         redirectUrl: `${url.protocol}//${url.host}/callback`
       },
       android: {
+        appId: 'speedmeetand',
         customScheme: 'nl.daanvanberkel.realtimepoc:/'
       },
       ios: {
+        appId: 'speedmeetios',
         customScheme: 'nl.daanvanberkel.realtimepoc:/'
       }
     }).then(response => {
       if (response.access_token) {
-        this.loginService.setAccessToken(response.access_token);
-        this.router.navigate(['/home']);
+        this.saveAccessToken(response.access_token);
       }
     }).catch(err => console.log('Error', err));
   }
 
+  private handleOpenUrl(data: AppUrlOpen|AppLaunchUrl): void {
+    const openUrl = new URL(data.url);
+    if (openUrl.searchParams.has('access_token')) {
+      this.saveAccessToken(openUrl.searchParams.get('access_token'));
+    }
+  }
+
+  private saveAccessToken(accessToken: string): void {
+    this.loginService.setAccessToken(accessToken);
+    this.router.navigate(['/home']);
+  }
+
+  private isAndroid(): boolean {
+    const platforms = this.platform.platforms();
+    return (platforms.indexOf('android') > -1);
+  }
 }
